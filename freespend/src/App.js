ddc1a3cebefb4de09f953d59489be4fabf26d64b
Marcus Smith <wariyor1@gmail.com>
2018-12-24 16:46:11 -0700

    Added header component and gave it a class to be used in css

--- freespend/src/App.js
+++ freespend/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from "./Header"
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
       </div>
     );
   }
---
+++ freespend/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+const Header = function(props){
+    return (<div>
+        <h1 className = "logo">FreeSpend.</h1>
+    </div>)
+}
+
+
+export default Header;
