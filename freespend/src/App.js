cda544e9f6a88ae2ae1d1938fbfedeecfc7acaac
Marcus Smith <wariyor1@gmail.com>
2018-12-25 19:32:33 -0700

    added an income click handler to App.js currenlty just getting it to push to the transactions array

--- freespend/src/App.js
+++ freespend/src/App.js
@@ -4,22 +4,38 @@ import Income from './Income';
 import Expense from './Expenses';
 import Total from './Total';
 
+function Cost(income, description){
+  this.amount = income.value;
+  this.description = description.value;
+}
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fixedExpense : [],
-      freeSpend : 0,
-      transactions : []
-    }
+      fixedExpense : [], // an array of objects, amount properties will be added up to determine the fixed expenses
+      freeSpend : 0,     // After fixed expenses and income are summed this will be what's left over.
+      transactions : [] // this will be an array of objects of the class Income, or Expense, they will contain the
+    }                   // date, the amount the type, and the description of the transaction
   }
+
+  
+
+  incomeClickHandler = () => {
+    let income = document.getElementById("income");
+    const expense = new Cost(income);
+    this.state.transactions.push(expense);
+  }
+
+
+
+
   render() {
     return (
       <div>
         <Header />
         <Income />
         <Expense />
-        <Total fixedExpense = '$3248' spent = "$249" freeSpend = "$380"/>
+        <Total fixedExpense = '$3248' spent = "$249" freeSpend = "$380"/> 
       </div>
     );
   }
--- freespend/src/Income.js
+++ freespend/src/Income.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+
 const Income = function(props){
     return (<div>
         <h1 className = "top">Income</h1>
