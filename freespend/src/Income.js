e8f04d4fa5ebc0d511a8da138ff28f65f70d09ef
Marcus Smith <wariyor1@gmail.com>
2018-12-26 20:59:09 -0700

    added alt attributes to all img tags

--- freespend/src/Income.js
+++ freespend/src/Income.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Income = function(props){
     return (<div className = "income">
-        <h1 className = "top">Income</h1><img src = "./img/icons8-cash-in-hand-80.png"></img>
+        <h1 className = "top">Income</h1><img alt = "a hand holding money" src = "./img/icons8-cash-in-hand-80.png"></img>
         <input id = "income"></input>
         <button id = "plus">+</button>
     </div>)
--- freespend/src/Nav.js
+++ freespend/src/Nav.js
@@ -4,10 +4,10 @@ import React from 'react';
 
 const Nav = function(props){
     return (<div className = "nav">
-        <div id = "expenses-button" className = "nav-button"><img src = "./img/icons8-debt-80.png"></img><p>Expenses</p></div>
-        <div id = "transaction-button" className = "nav-button"><img src = "./img/icons8-ledger-80.png"></img><p>Transactions</p></div>
-        <div id = "goal-button" className = "nav-button"><img src = "./img/icons8-prefab-house-80.png"></img><p>Goals</p></div>
-        <div id = "send-button" className = "nav-button"><img src = "./img/icons8-exchange-80.png"></img><p>Send/Recieve</p></div>
+        <div id = "expenses-button" className = "nav-button"><img src = "./img/icons8-debt-80.png" alt = "a money sign on a heavy weight"></img><p>Expenses</p></div>
+        <div id = "transaction-button" className = "nav-button"><img src = "./img/icons8-ledger-80.png" alt = "a book or ledger with a money sign in it"></img><p>Transactions</p></div>
+        <div id = "goal-button" className = "nav-button"><img src = "./img/icons8-prefab-house-80.png" alt = "a house icon"></img><p>Goals</p></div>
+        <div id = "send-button" className = "nav-button"><img src = "./img/icons8-exchange-80.png"alt = " a send or recieve money icon"></img><p>Send/Recieve</p></div>
         </div>)
 }
 
