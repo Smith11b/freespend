67af6df3a38ac5b3798934e2e50f516466880b99
Marcus Smith <wariyor1@gmail.com>
2018-12-26 19:34:49 -0700

    finished styling on current components, need touch up but app is looking good.

--- freespend/src/Total.js
+++ freespend/src/Total.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Total = function(props){
     return (<div className = "freespend-total">
-        <h3>Fixed Expenses:</h3><h1>{props.fixedExpense}</h1>
-        <h3>FreeSpend Spent:</h3><h1>{props.spent}</h1>
-        <h1>FreeSpend:</h1><h1>{props.freeSpend}</h1>
+        <div><h3 className = "total-label">Fixed Expenses:</h3><h1 className = "total-amount">{props.fixedExpense}</h1></div>
+        <div><h3 className = "total-label">FreeSpend Spent:</h3><h1 className = "total-amount">{props.spent}</h1></div>
+        <div><h1 className = "total-label" id = "spend-label">FreeSpend:</h1><h1 className = "total-amount" id = "spend">{props.freeSpend}</h1></div>
     </div>)
 }
 
