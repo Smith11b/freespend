69f197c4c2a62407877bfc8627bff7845edb8310
Marcus Smith <wariyor1@gmail.com>
2018-12-28 21:13:49 -0700

    added comments to functions in app.js

--- freespend/src/App.js
+++ freespend/src/App.js
@@ -22,11 +22,17 @@ class App extends Component {
       expenseInput : '',
       fixedExpenses : 0
        }
+
+       // Bindings for passed functions ---------------------
        this.incomeOnChangeHandler = this.incomeOnChangeHandler.bind(this)   
        this.incomeClickHandler = this.incomeClickHandler.bind(this)
        this.transactTotal = this.transactTotal.bind(this)   
        this.calculateFreeSpend = this.calculateFreeSpend.bind(this)            
   }
+
+  // This function adds a new revenue transaction to the transactions array in state, then calls calculate freespend which also
+  // updates state. I'll need to refactor this. The console log was for debuggin purposes
+
 incomeClickHandler = () => {
    const rev = new Revenue(this.state.incomeInput);
    this.setState( state =>{
@@ -41,6 +47,9 @@ incomeClickHandler = () => {
   console.log(this.state.transactions);
   }
 
+  // This function goes through the array of fixed expenses which are negative intergers, then ads them together to get the
+  //change the fixed expenses amount in state. I'll need to refactor it (it's redundant) and it will also need to update state
+
   calulateFixedExpenses = () => {
     let result = 0
     for(let i = 0; i < this.state.fixedExpenses.length; i++) {
@@ -50,6 +59,9 @@ incomeClickHandler = () => {
 
   }
 
+  // this function does the same thing, but with the transactions array. I'm writing it hodgpodge right now because of time
+  // constraints, but will refactor these last two functions into  a single one.
+
   transactTotal = () => {
     let result = 0
     for(let i = 0; i < this.state.transactions.length; i++) {
@@ -58,6 +70,10 @@ incomeClickHandler = () => {
     return result;
 }
 
+//this function updates freespend by taking the transaction total (positive or negative depending on how the user has transacted
+// and ads it to the fixed expenses totaled in state. It does this while updating the freespend property of state so it can be displayed
+// to the user.
+
   calculateFreeSpend = () => {
     let total = this.transactTotal()
     let expense = this.state.fixedExpense
@@ -65,9 +81,16 @@ incomeClickHandler = () => {
 
   }
 
+  // This handles the usesr input on the income component. Allows it to update state so the data from the user can be used in other
+  // places in the application
+
   incomeOnChangeHandler = (e) => {
     this.setState({incomeInput : e.target.value});
   }
+
+  // I've broken the render function down into it's components. Total currently has placeholder values but I'll need to refactor those
+  // to update when state does. 
+
   render() {
     return (
       <div>
