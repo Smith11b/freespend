3327d2db9eca31aa5b998921242f6cddc7e6f4dc
Marcus Smith <wariyor1@gmail.com>
2019-01-16 16:31:06 -0700

    added readme image

--- freespend/src/Transactions.js
+++ freespend/src/Transactions.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import TransactionCard from './TransactionCard';
 
 function Transactions(props) {
     return (
         <div>
-            
+            {props.transactions.map(transaction => {
+                return <TransactionCard amount = {props.amount} description = {props.description} />
+            })}
         </div>
     )
 }
