d3087820f7e2b13d62898f6914e0a412c7cbf99d
Marcus Smith <wariyor1@gmail.com>
2019-01-17 15:28:26 -0700

    added transaction header card, needs css

---
+++ freespend/src/TransactionHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AddFixedExp = function(props) {
+  return (
+    <div className="transaction-head" >
+      <h1 className="top transaction-h1">Transactions</h1>
+      <img
+        alt="large home icon"
+        src="./img/icons8-ledger-80.png"
+      />
+    </div>
+  );
+};
+
+export default AddFixedExp;
--- freespend/src/Transactions.js
+++ freespend/src/Transactions.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import TransactionCard from './TransactionCard';
+import TransactionHeader from "./TransactionHeader";
 
 function Transactions(props) {
     return (
         <div>
+            <TransactionHeader />
+        <div className = "transaction-mid">
             {props.transactions.map(transaction => {
                 return <TransactionCard amount = {transaction.amount} description = {transaction.description} />
             })}
+        </div>
         </div>
     )
 }
