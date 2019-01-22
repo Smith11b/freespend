be4fb7e6df50c0659825a32e4f9893760b1154f6
Marcus Smith <wariyor1@gmail.com>
2019-01-21 17:59:15 -0700

    finished transaction card css

--- freespend/src/TransactionCard.js
+++ freespend/src/TransactionCard.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function TransactionCard(props) {
-    return (
-        <div className = "transaction-card">
-            <h1 className = "transaction-entry">{`${props.description} :`}</h1><img className = "edit-icon" alt = "edit icon" src="https://img.icons8.com/material/24/000000/edit.png"></img><img alt = "delete icon" className = "edit-icon" src="https://img.icons8.com/material-outlined/24/000000/close-window.png"></img><h1 className = "transaction-amount">{`$${props.amount}`}</h1>
-        </div>
-    )
+    const style = {
+  color : props.amount > 0 ? "#4cefa4" : "#ffa50a"
+    };
+    const plus = props.amount > 0 ? "+" : "";
+  return (
+    <div className="transaction-card">
+      <h1 className="transaction-entry">{`${props.description} :`}</h1>
+      <img
+        className="edit-icon"
+        alt="edit icon"
+        src="https://img.icons8.com/material/24/000000/edit.png"
+      />
+      <img
+        alt="delete icon"
+        className="edit-icon delete"
+        src="https://img.icons8.com/material-outlined/24/000000/close-window.png"
+      />
+      <h1 style = {style} className="transaction-amount">{`${plus}$${props.amount}`}</h1>
+    </div>
+  );
 }
 
 export default TransactionCard;
