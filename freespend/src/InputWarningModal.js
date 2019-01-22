b03a727ec0285fecb7704b49df0ac520ab13c7ef
Marcus Smith <wariyor1@gmail.com>
2019-01-21 18:12:40 -0700

    added css for warning modal and goal button

--- freespend/src/InputWarningModal.js
+++ freespend/src/InputWarningModal.js
@@ -6,7 +6,7 @@ function InputWarningModal(props) {
       style={!props.show ? { display: "none" } : { display: "block" }}
       className="modal-overlay"
     >
-      <div className="modal-box">
+      <div className="modal-box warning-modal">
         <h1>You entered that incorrectly, try again</h1>
         <button onClick={props.toggleInputWarning}>Ok</button>
       </div>
